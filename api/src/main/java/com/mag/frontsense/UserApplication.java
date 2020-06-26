1d914809e14b43f56cda8217cf64a6076c329478
asrsa <aljaz.srsa@gmail.com>
2020-06-26 15:16:02 +0200

    added service registring etcd

--- api/src/main/java/com/mag/frontsense/UserApplication.java
+++ api/src/main/java/com/mag/frontsense/UserApplication.java
@@ -1,10 +1,11 @@
 package com.mag.frontsense;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.OPTIONS;
 import javax.ws.rs.core.Application;
 
+@RegisterService
 @ApplicationPath("/")
 public class UserApplication extends Application {
 
